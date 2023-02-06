d904afcf1719464b4d62c90910749f5085ceca78
Victor Piolin <victor.piolin@gmail.com>
2023-02-05 23:41:05 -0500

    Add default test

--- transaction_test.go
+++ transaction_test.go
@@ -20,3 +20,17 @@ func TestSimpleTransaction(t *testing.T) {
 
 	assert.Equal(t, float64(100), transac.reference.value)
 }
+
+func TestDefaultTransactionRef(t *testing.T) {
+	transac := newTransaction(asset{
+		quantiy:  1,
+		code:     "BTC",
+		isCrypto: true,
+	}, asset{
+		quantiy:  100,
+		code:     "ETH",
+		isCrypto: true,
+	}, time.Now().AddDate(1993, 10, 03))
+
+	assert.Equal(t, float64(0), transac.reference.value)
+}
